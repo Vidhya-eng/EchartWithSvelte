9e24821a9b4d0b620165ee5a2a064bb229158e79
unknown <vid2dhan@gmail.com>
2021-04-13 09:08:09 +0200

    First Commit

---
+++ svelte-typescript-app/src/Chart.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import * as echarts from "echarts";
+let chart = echarts.init(<HTMLDivElement>document.getElementById("app"), {});
+
+chart.setOption({
+    title: {
+        text: 'ECharts Getting Started Example'
+    },
+    tooltip: {},
+    xAxis: {
+        data: ['shirt', 'cardigan', 'chiffon', 'pants', 'heels', 'socks']
+    },
+    yAxis: {},
+    series: [{
+        name: 'sales',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+});
+
+
+</script>
+  
+  <div id="main" style="width: 600px;height:400px;"></div>
